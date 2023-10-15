fedc805a1af9ce045306cb7f78e1ee9cbfcfe728
Shagun Bandi <shagunbandi@ShagunsookM1Pro.home>
2023-10-16 00:08:07 +0200

    api for itinerary

--- backend/planmytrip_backend/itinerary/urls.py
+++ backend/planmytrip_backend/itinerary/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from .views import ItineraryListApiView, ItineraryDetailApiView
+
+urlpatterns = [
+    path("api", ItineraryListApiView.as_view()),
+    path("api/<int:itinerary_id>/", ItineraryDetailApiView.as_view()),
+]
--- backend/planmytrip_backend/itinerary/views.py
+++ backend/planmytrip_backend/itinerary/views.py
@@ -35,3 +35,72 @@ class ItineraryListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItineraryDetailApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    errors = {"itinerary_does_not_exist": "Object with itinerary id does not exists"}
+
+    # 3. Retrieve
+    def get(self, request, itinerary_id, *args, **kwargs):
+        """
+        Retrieves the Itinerary with given itinerary_id
+        """
+        itinerary_instance = self._get_object(itinerary_id, request.user.id)
+        if not itinerary_instance:
+            return Response(
+                {"res": self.errors["itinerary_does_not_exist"]},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = ItinerarySerializer(itinerary_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 4. Update
+    def put(self, request, itinerary_id, *args, **kwargs):
+        """
+        Updates the itinerary with given itinerary_id if exists
+        """
+        itinerary_instance = self._get_object(itinerary_id, request.user.id)
+        if not itinerary_instance:
+            return Response(
+                {"res": self.errors["itinerary_does_not_exist"]},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        data = {
+            "name": request.data.get("name"),
+            "description": request.data.get("description"),
+            "user": request.user.id,
+        }
+        serializer = ItinerarySerializer(
+            instance=itinerary_instance, data=data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 5. Delete
+    def delete(self, request, itinerary_id, *args, **kwargs):
+        """
+        Deletes the itinerary with given itinerary_id if exists
+        """
+        itinerary_instance = self._get_object(itinerary_id, request.user.id)
+        if not itinerary_instance:
+            return Response(
+                {"res": self.errors["itinerary_does_not_exist"]},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        itinerary_instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def _get_object(self, itinerary_id, user_id):
+        """
+        Helper method to get the object with given itinerary_id, and user_id
+        """
+        try:
+            return Itinerary.objects.get(id=itinerary_id, user=user_id)
+        except Itinerary.DoesNotExist:
+            return None
