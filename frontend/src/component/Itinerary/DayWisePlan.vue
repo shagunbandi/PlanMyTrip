2ca0110bdf16815619232a9727891ef3142330b6
Shagun Bandi <shagunbandi@ShagunsookM1Pro.home>
2024-01-23 00:32:07 +0100

    Day component

--- frontend/src/component/Itinerary/DayWisePlan.vue
+++ frontend/src/component/Itinerary/DayWisePlan.vue
@@ -1,56 +1,23 @@
 <template>
   <h4 class="day-wise-plan">Day-wise plan</h4>
-  <div class="d-flex" v-for="day in days" :key="day.id">
-    <div class="flex-grow-1">
-      <h5 class="day d-flex">
-        <span class="day-text flex-shrink-0"
-          >Day {{ day.order + 1 }}:&nbsp;</span
-        >
-        <InPlaceEditableInput
-          inputType="input"
-          placeholder="Write a title for you day"
-          itemKey="name"
-          :value="day.name"
-          :editEndPoint="`/day/api/${day.id}/?itinerary_id=${itineraryId}`"
-        />
-      </h5>
-      <p>
-        <label for="notes">Notes</label>
-        <InPlaceEditableInput
-          :value="day.notes"
-          inputType="textarea"
-          placeholder="Write something about the day"
-          :editEndPoint="`/day/api/${day.id}/?itinerary_id=${itineraryId}`"
-          itemKey="notes"
-        />
-      </p>
-      <br />
-    </div>
-    <div class="button-container flex-shrink-0">
-      <button class="transparent-btn cross" @click="removeDay(day.id)">
-        X
-      </button>
-      <button class="transparent-btn move-up" @click="moveDay(day.id, 'up')">
-        ↑
-      </button>
-      <button
-        class="transparent-btn move-down"
-        @click="moveDay(day.id, 'down')"
-      >
-        ↓
-      </button>
-    </div>
-  </div>
+  <Day
+    v-for="day in days"
+    :key="day.id"
+    :itineraryId="itineraryId"
+    :day="day"
+    @moveDay="moveDay"
+    @removeDay="removeDay"
+  />
   <button class="btn btn-success" @click="addDay">Add Day</button>
 </template>
 
 <script>
 import api from '@/api'
-import InPlaceEditableInput from '@/component/InPlaceEditableInput.vue'
+import Day from '@/component/Itinerary/Day.vue'
 
 export default {
   components: {
-    InPlaceEditableInput,
+    Day,
   },
   props: {
     itineraryId: Number,
@@ -107,27 +74,5 @@ export default {
 /* Day Wise Plan */
 .day-wise-plan {
   font-weight: bold;
-}
-
-.day {
-  font-style: italic;
-  display: flex;
-  width: 100%;
-}
-
-/* Button Group */
-
-.button-container {
-  display: flex;
-  flex-flow: column;
-  padding-left: 4px;
-}
-
-.button-container button {
-  background-color: white;
-  border: none;
-}
-.cross {
-  color: red;
 }
 </style>
