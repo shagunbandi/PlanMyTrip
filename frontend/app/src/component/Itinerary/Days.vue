34df2acdfbfa70316866a93a915a46e9c09f1ad3
Shagun Bandi <shagunbandi@ShagunsookM1Pro.home>
2024-01-30 21:52:09 +0100

    Attraction

--- frontend/app/src/component/Itinerary/Days.vue
+++ frontend/app/src/component/Itinerary/Days.vue
@@ -1,38 +1,105 @@
 <template>
   <h4 class="day-wise-plan">Day-wise plan</h4>
-  <Day
-    v-for="day in days"
-    :key="day.id"
-    :itineraryId="itineraryId"
-    :day="day"
-    @moveDay="moveDay"
-    @removeDay="removeDay"
-  />
+  <div v-for="day in days" :key="day.id" class="d-flex">
+    <div class="flex-grow-1">
+      <h5 id="day-name" class="day d-flex">
+        <span class="day-text flex-shrink-0"
+          >Day {{ day.order + 1 }}:&nbsp;</span
+        >
+        <InPlaceEditableInput
+          inputType="input"
+          placeholder="Write a title for you day"
+          itemKey="name"
+          :value="day.name"
+          :editEndPoint="`/api/day/${day.id}/?itinerary_id=${itineraryId}`"
+        />
+      </h5>
+      <span id="notes">
+        <label for="notes">Notes</label>
+        <InPlaceEditableInput
+          :value="day.notes"
+          inputType="textarea"
+          placeholder="Write something about the day"
+          :editEndPoint="`/api/day/${day.id}/?itinerary_id=${itineraryId}`"
+          itemKey="notes"
+        />
+      </span>
+      <br />
+      <Detail :dayId="day.id" :details="day.dishes" name="dish" title="Dish" />
+      <Detail
+        :dayId="day.id"
+        :details="day.accomodations"
+        name="accomodation"
+        title="Accomodation"
+      />
+
+      <Detail
+        :dayId="day.id"
+        :details="day.restaurants"
+        name="restaurant"
+        title="Restaurant"
+      />
+
+      <Detail
+        :dayId="day.id"
+        :details="day.attractions"
+        name="attraction"
+        title="Attraction"
+        :newDetailData="newAttraction"
+      />
+
+      <!-- All other features go here -->
+    </div>
+
+    <div class="button-container flex-shrink-0">
+      <button class="cross" @click="removeDay(day.id)">X</button>
+      <button
+        class="move-up"
+        @click="moveDay({ dayId: day.id, moveDirection: 'up' })"
+      >
+        ↑
+      </button>
+      <button
+        class="move-down"
+        @click="moveDay({ dayId: day.id, moveDirection: 'down' })"
+      >
+        ↓
+      </button>
+    </div>
+  </div>
   <button class="btn btn-success" @click="addDummyDay">Add Day</button>
 </template>
 
 <script>
-import Day from '@/component/Itinerary/Day.vue'
+import InPlaceEditableInput from '@/component/InPlaceEditableInput.vue'
+import Detail from '@/component/Itinerary/Detail.vue'
+import { ATTRACTION_ENUM } from '@/constants'
 import { mapActions } from 'vuex'
 
 export default {
   components: {
-    Day,
+    InPlaceEditableInput,
+    Detail,
   },
   props: {
     itineraryId: Number,
     days: Object,
   },
   data() {
-    return {}
+    return {
+      newAttraction: {
+        name: 'New Attraction',
+        notes: '',
+        category: ATTRACTION_ENUM.EXPERIENCE,
+      },
+    }
   },
   methods: {
     addDummyDay() {
-      const newDayData = {
+      this.addDay({
         name: 'New Day',
         notes: '',
-      }
-      this.addDay(newDayData)
+      })
     },
     ...mapActions('itinerary', [
       'fetchItinerary',
@@ -47,5 +114,26 @@ export default {
 <style scoped>
 .day-wise-plan {
   font-weight: bold;
+}
+
+.day {
+  font-style: italic;
+  display: flex;
+  width: 100%;
+}
+
+/* Button Group */
+.button-container {
+  display: flex;
+  flex-flow: column;
+  padding-left: 4px;
+}
+
+.button-container button {
+  background-color: white;
+  border: none;
+}
+.cross {
+  color: red;
 }
 </style>
--- frontend/app/src/store/actions/dayActions.js
+++ frontend/app/src/store/actions/dayActions.js
@@ -1,41 +1,30 @@
 // dayActions.js
 import api from '@/api'
 
-export const addDay = async ({ commit, state }, newDay) => {
+export const addDay = async ({ state, dispatch }, newDay) => {
   const itineraryId = state.itinerary.id
 
   try {
-    const response = await api.post(
-      `/api/itinerary/${itineraryId}/day/`,
-      newDay,
-    )
-    const updatedItinerary = { ...state.itinerary }
-    updatedItinerary.days.push(response.data)
-
-    commit('SET_ITINERARY', updatedItinerary)
+    await api.post(`/api/itinerary/${itineraryId}/day/`, newDay)
+    dispatch('fetchItinerary', itineraryId)
   } catch (error) {
     console.error('Error adding day:', error)
   }
 }
 
-export const removeDay = async ({ commit, state }, dayId) => {
+export const removeDay = async ({ state, dispatch }, dayId) => {
   const itineraryId = state.itinerary.id
 
   try {
     await api.delete(`/api/itinerary/${itineraryId}/day/${dayId}/`)
-    const updatedItinerary = { ...state.itinerary }
-    updatedItinerary.days = updatedItinerary.days.filter(
-      (day) => day.id !== dayId,
-    )
-
-    commit('SET_ITINERARY', updatedItinerary)
+    dispatch('fetchItinerary', itineraryId)
   } catch (error) {
     console.error('Error removing day:', error)
   }
 }
 
 export const moveDay = async (
-  { commit, state, dispatch },
+  { state, dispatch },
   { dayId, moveDirection },
 ) => {
   const itineraryId = state.itinerary.id
@@ -47,6 +36,5 @@ export const moveDay = async (
     dispatch('fetchItinerary', itineraryId)
   } catch (error) {
     console.error('Error moving day:', error)
-    commit('SET_ERROR', error.message || 'Error moving day')
   }
 }
